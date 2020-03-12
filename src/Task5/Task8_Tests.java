1e0eabf580fc120c8c8bff05d8737fd9475da8ba
samoha01 <samoha1@list.ru>
2020-03-12 13:23:31 +0300

    Добавил тесты

---
+++ src/Task5/Task8_Tests.java
@@ -0,0 +1,48 @@
+package Task5;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * @author Samohin Alexander
+ */
+
+public class Task8_Tests {
+
+    @Test
+    public void doSum() {
+        Slozhenie slozhenie = new Slozhenie(8, 4.5, 1);
+        double res = slozhenie.doSum();
+        assertEquals("Не прошла проверка оператора сложения", 12.5, res);
+    }
+
+    @Test
+    public void doVich() {
+        Vichitanie vichitanie = new Vichitanie(10, 8, 2);
+        double res = vichitanie.doVich();
+        assertEquals("Не прошла проверка оператора вычитания", 2.0, res);
+    }
+
+    @Test
+    public void doDel() {
+        Delenie delenie = new Delenie(84, 2, 3);
+        double res = delenie.doDel();
+        assertEquals("Не прошла проверка оператора деления", 42.0, res);
+    }
+
+    @Test
+    public void doUmn() {
+        Umnozhenie umnozhenie = new Umnozhenie(3.8, 5, 4);
+        double res = umnozhenie.doUmn();
+        assertEquals("Не прошла проверка оператора умножения", 19.0, res);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivideByZero()throws ArithmeticException {
+        Delenie delenie = new Delenie(84, 0, 3);
+        double res = delenie.doDel();
+        Assert.fail("Не прошла проверка оператора " + res);
+    }
+}
